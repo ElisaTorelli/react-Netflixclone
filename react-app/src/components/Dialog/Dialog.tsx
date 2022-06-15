917ced2784dc95d7500a00de64fab736707a61bd
Diakoretina <francesco.tammaro2000@gmail.com>
2022-06-16 01:17:50 +0200

    Add js on Dialog

--- react-app/src/components/Dialog/Dialog.tsx
+++ react-app/src/components/Dialog/Dialog.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import styles from './dialog.module.css'
 
+let arMovie = [{titolo:"Questione di chimica",duration:"58",synopsis:"Dopo la diagnosi di cancro terminale ai polmoni, un insegnante di chimica del liceo si dà alla produzione di metanfetamine per garantire la sopravvivenza della  famiglia."},
+{titolo:"Senza ritorno",duration:"48",synopsis:"Il primo traffico di droga fallito costringe Walt e Jesse a sbarazzarsi di un paio di cadaveri. Nel frattempo, Skyler sospetta che il marito sia sulla cattiva strada."},
+{titolo:"Conseguenze radicali",duration:"48",synopsis:"Mentre Walt rimette le cose a posto dopo il primo traffico di droga, Skyler è vicinissima a scoprire la verità sulla sua doppia vita."},
+{titolo:"Una malattia scomoda",duration:"48",synopsis:"Costretto a rivelare la verità sulla malattia che lo affligge, Walt si chiede come pagare i costosi trattamenti contro il cancro ai quali si deve sottoporre."},
+{titolo:"Materia grigia",duration:"48",synopsis:"Skyler cerca di convincere Walt ad accettare la generosa offerta di un ex migliore amico disposto a pagargli le cure per il cancro."},
+{titolo:"Un pugno di mosche",duration:"48",synopsis:"Per finanziare le crescenti spese mediche del suo trattamento, Walt chiede a Jesse di trovare un acquirente per la droga. Per farlo, Jesse si mette nei guai."},
+{titolo:"Vendetta",duration:"47",synopsis:"Una volta che Jesse è scampato alla morte, Walt accetta di produrre ancor più droga per lo spietato Tuco. Nel frattempo, Skyler sospetta la sorella di cleptomania."}];
+
+let arSelection = [{value:"stagione1",option:"Satgione 1"},
+{value:"stagione2",option:"Satgione 2"},
+{value:"stagione3",option:"Satgione 3"},
+{value:"stagione4",option:"Satgione 4"},]
+
 const Dialog = () => {
   return (
     <div className={styles.container}>
@@ -9,26 +22,33 @@ const Dialog = () => {
                 <h3>Episodi</h3>
                 <div className={styles.shows}>
                     <select name='show' id='show' className={styles.show}>
-                        <option value='stagione1'>Stagione 1</option>
-                        <option value='stagione2'>Stagione 2</option>
-                        <option value='stagione3'>Stagione 3</option>
-                        <option value='stagione4'>Stagione 4</option>
+                      {
+                        arSelection.map((item,index) =>{
+                          return(
+                            <option value={item.value}>{item.option}</option>
+                          )
+                        })
+                      }
                     </select>
                 </div> 
             </div>
             <div className={styles.mid} id='mid'>
-                <div className={styles.episode}>
-                    <h3>1</h3>
+              {
+                arMovie.map((item,index) =>{
+                  return(
+                    <div key={'movie'+index} className={styles.episode}>
+                    <h3>{index+1}</h3>
                     <img src={require('../../assets/images/New-amsterdam.png')} alt='CardImage'/>
                     <div className={styles.description}>
-                        <h4>Questione di chimica</h4>
-                        <p>58min</p>
-                        <p>Dopo la diagnosi di cancro terminale ai polmoni, un insegnante di chimica del
-                            liceo si da alla produzione di metanfetamina per garanatire la sopravivvenza della
-                            famiglia.
+                        <h4>{item.titolo}</h4>
+                        <p>{item.duration}min</p>
+                        <p>{item.synopsis}
                         </p>
                     </div>
                 </div>
+                  )
+                })
+              }
             </div>
         </div>
     </div>
