8f3688f5be1cc6d38407a78720f2cf353232a88e
Justy116 <giustinogragnaniello116@gmail.com>
2022-06-15 15:06:20 +0200

    Add footer

--- react-app/src/components/Row/Row.tsx
+++ react-app/src/components/Row/Row.tsx
@@ -1,35 +1,38 @@
 import styles from "./row.module.css"
+import socialImg from "../../assets/images/Social.png"
+import social from "../../assets/images/grayBorder.png"
+
 
 
 const Row = () => {
   return (
     <div>
       {/* -- LAST SECTION LINKS  */}
-      <footer className="footer">
+      <footer className={styles["footer"]}>
         {/* -- SOCIAL MEDIA ICONS  */}
-        <div className="last__section__image">
-          {/* <img src="./assets/images/Social.png" alt="social logo img"> */}
+        <div className={styles["last__section__image"]}>
+           <img src={socialImg} alt="social logo img" /> 
         </div>
 
-        <div className="footer-cols">
+        <div className={styles["footer-cols"]}>
           <ul>
             <li><a href="#">Autodescrizione</a></li>
             <li><a href="#">Rapporti con gli investitori</a></li>
             <li><a href="#">Note legali</a></li>
             <li><a href="#">Bize ulasin</a></li>
-            {/* <li><a href="#" id="gray_box"><img src="./assets/images/grayBorder.png" alt="gray icon"></a></li> */}
+            <li><a href="#" id={styles["gray_box"]}><img src={social} alt="gray icon" /></a></li>
           </ul>
-          <ul className="second_link">
+          <ul className={styles["second_link"]}>
             <li><a href="#">Centro assistenza</a></li>
             <li><a href="#">Opportunità di lavoro</a></li>
             <li><a href="#">Preferenze per i cookie</a></li>
           </ul>
-          <ul className="second_link">
+          <ul className={styles["second_link"]}>
             <li><a href="#">Carte Regalo</a></li>
             <li><a href="#">Condizioni di utilizzo</a></li>
             <li><a href="#">Informazioni sull'azienda</a></li>
           </ul>
-          <ul className="second_link">
+          <ul className={styles["second_link"]}>
             <li><a href="#">Media Center</a></li>
             <li><a href="#">Privacy</a></li>
             <li><a href="#">Contattaci</a></li>
