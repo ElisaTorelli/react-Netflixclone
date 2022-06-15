f38ad3c32c4e33e35fd311f6777e309b531a969c
Diakoretina <francesco.tammaro2000@gmail.com>
2022-06-15 17:03:28 +0200

    Fixed Dialog

--- react-app/src/components/Dialog/Dialog.tsx
+++ react-app/src/components/Dialog/Dialog.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import "./Dialogstyle.css"
+import styles from './dialog.module.css'
 
 const Dialog = () => {
   return (
-    <div className="container">
-        <div className="space">
-            <div className="top">
+    <div className={styles.container}>
+        <div className={styles.space}>
+            <div className={styles.top}>
                 <h3>Episodi</h3>
-                <div className="shows">
-                    <select name="show" id="show" className="show">
-                        <option value="stagione1">Stagione 1</option>
-                        <option value="stagione2">Stagione 2</option>
-                        <option value="stagione3">Stagione 3</option>
-                        <option value="stagione4">Stagione 4</option>
+                <div className={styles.shows}>
+                    <select name='show' id='show' className={styles.show}>
+                        <option value='stagione1'>Stagione 1</option>
+                        <option value='stagione2'>Stagione 2</option>
+                        <option value='stagione3'>Stagione 3</option>
+                        <option value='stagione4'>Stagione 4</option>
                     </select>
                 </div> 
             </div>
-            <div className="mid" id="mid">
-                <div className="episode">
+            <div className={styles.mid} id='mid'>
+                <div className={styles.episode}>
                     <h3>1</h3>
-                    <img src={require("../../assets/images/New-amsterdam.png")} alt="CardImage"/>
-                    <div className="description">
+                    <img src={require('../../assets/images/New-amsterdam.png')} alt='CardImage'/>
+                    <div className={styles.description}>
                         <h4>Questione di chimica</h4>
                         <p>58min</p>
                         <p>Dopo la diagnosi di cancro terminale ai polmoni, un insegnante di chimica del
