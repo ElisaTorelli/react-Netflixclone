db9603786e676f4a4be4247dee637fab5db0b8c6
MarikaMorittu <marikaegio@hotmail.it>
2022-06-16 16:17:48 +0200

    API commit

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,28 +1,37 @@
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import RowPoster from "./components/RowPoster/RowPoster";
 import Row from "./components/Row/Row";
 import RowNumber from "./components/RowNumber/RowNumber";
 import Header from "./components/Header/Header";
+import { IMovie } from "./commons/IResult";
 import Footer from "./components/Footer/Footer";
-import Index from "./components/Index/Index";
 
+const App: React.FC<{}> = () => {
+  const [data, setData] = useState<Array<IMovie> | []>([]);
 
-function App() {
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=3bb82372ef97dc8c0c45d353261e2f31&quot"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setData([...data.results]);
+      });
+  }, []);
+
   return (
     <div className="App">
-      {/* <Navbar/>
+      <Navbar />
       <Header />
-      <Row />
-      <Row />
-      <Row />
+      <Row title="Film Popolari" movieList={data} />
+      <Row title="Film Popolari" movieList={data} />
+      <Row title="Film Popolari" movieList={data} />
       <RowPoster />
       <RowNumber />
-      <Row />
-      <Row />
-      <Footer /> */}
-      <Index />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
